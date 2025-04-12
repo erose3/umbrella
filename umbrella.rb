f3372810b00578b44d569e6a0c3f0343f964e920
erose3 <eli.rosenthal3@gmail.com>
2025-04-12 17:45:53 +0000

    final

--- umbrella.rb
+++ umbrella.rb
@@ -9,17 +9,17 @@ weather_key = ENV.fetch("WEATHER_KEY")
 
 pp "Hi! Where are you?"
 
-# user_location = gets.chomp
+user_location = gets.chomp
 
-user_location = "Chicago"
+# user_location = "New York"
 
-pp user_location
+# pp user_location
 
 gmap_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + gmaps_key
 
 # https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key=REPLACE_THIS_QUERY_STRING_PARAMETER_WITH_YOUR_API_TOKEN
 
-pp gmap_url
+# pp gmap_url
 
 raw_response = HTTP.get(gmap_url)
 
@@ -41,8 +41,8 @@ long_float = location.fetch ("lng").to_s
 lat = lat_float.to_s
 long = long_float.to_s
 
-pp lat
-pp long
+# pp lat
+# pp long
 
 weather_url = "https://api.pirateweather.net/forecast/" + weather_key + "/" + lat + "," + long
 
@@ -52,5 +52,27 @@ weather_url = "https://api.pirateweather.net/forecast/" + weather_key + "/" + la
 weather_response = HTTP.get(weather_url)
 
 parsed_weather = JSON.parse(weather_response)
+hourly = parsed_weather.fetch("hourly")
+data = hourly.fetch("data")
 
-pp parsed_weather.keys
+hour0 = data.at(0)
+
+hour0_summary = hour0.fetch("summary")
+hour0_rain = hour0.fetch("precipProbability")
+
+temp = hour0.fetch("temperature")
+
+# pp temp
+# pp hour0_summary
+
+# pp hour0_rain
+
+if hour0_rain > 0
+    pp "The forecast right now says '#{hour0_summary}'. You should bring an umbrella!"
+else
+    pp "The forecast right now says '#{hour0_summary}'. You should be good to go!"
+end
+
+if temp < 60
+  pp "The temperature is #{temp} so you might want to bring a jacket too."
+end
